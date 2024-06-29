a545e63ded0270069a9c009d43203c95194ef0d4
Dmitry Ponyatov <dponyatov@gmail.com>
2024-06-29 18:31:10 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- inc/bcx.hpp
+++ inc/bcx.hpp
@@ -1,3 +1,6 @@
+/// @file
+/// @brief 32-bit FORTH Virtual Machine
+
 #pragma once
 
 /// Memory size, bytes
@@ -12,9 +15,18 @@
 #include <assert.h>
 #include <stdint.h>
 
+/// @brief program entry point
 extern int main(int argc, char *argv[]);
+/// @brief dump command line argument
 extern void arg(int argc, char argv[]);
 
 extern uint8_t M[Msz];   ///< main Memory, byte addressed
+extern uint32_t Cp;      ///< compiler pointer / allocator
+extern uint32_t Ip;      ///< instruction pointer
+extern uint8_t op;       ///< current opcode
 extern uint32_t R[Rsz];  ///< Return stack
+extern uint16_t Rp;      ///< return stack pointer
 extern int32_t D[Dsz];   ///< Data stack
+extern uint8_t Dp;       ///< data stack pointer
+
+extern int vm();  ///< bytecode interpreter
--- src/bcx.cpp
+++ src/bcx.cpp
@@ -5,9 +5,31 @@ int main(int argc, char* argv[]) {
     for (int i = 1; i < argc; i++) {  //
         arg(i, argv[i]);
     }
-    return 0;
+    return vm();
 }
 
 void arg(int argc, char argv[]) {  //
     fprintf(stderr, "argv[%i] = <%s>\n", argc, argv);
+}
+
+uint8_t M[Msz];
+uint32_t Cp = 0;
+uint32_t Ip = 0;
+uint8_t op = 0;
+uint32_t R[Rsz];
+uint16_t Rp = 0;
+int32_t D[Dsz];
+uint8_t Dp = 0;
+
+int vm() {
+    op = M[Ip++];
+    assert(Ip < Msz);
+    fprintf(stderr, "%.4X: %.2X\t", Ip - 1, op);
+    switch (op) {
+        default:
+            fprintf(stderr, "???\n\n");
+            abort();
+    }
+    fprintf(stderr, "\n");
+    return 0;
 }
