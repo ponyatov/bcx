fee8482841610983c96b0bbd586129d575de2dad
Dmitry Ponyatov <dponyatov@gmail.com>
2024-07-01 09:59:32 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/cclibs.mk
+++ mk/cclibs.mk
@@ -1,6 +1,10 @@
-GCCLIBS_CFG = --disable-shared
-GMP_CFG     = $(GCCLIBS_CFG)
-MPFR_CFG    = $(GCCLIBS_CFG) --with-gmp=$(CROSS)
+GCCLIBS_CFG  = --disable-shared
+GCCLIBS_WITH = --with-gmp=$(CROSS) --with-mpfr=$(CROSS) --with-mpc=$(CROSS)
+GMP_CFG     = $(GCCLIBS_CFG) $(GCCLIBS_WITH)
+MPFR_CFG    = $(GCCLIBS_CFG) $(GCCLIBS_WITH)
+MPC_CFG     = $(GCCLIBS_CFG) $(GCCLIBS_WITH)
+
+cclibs: gmp mpfr mpc
 
 gmp: $(CROSS)/lib/libgmp.a
 $(CROSS)/lib/libgmp.a: $(TMP)/$(GMP)/README
