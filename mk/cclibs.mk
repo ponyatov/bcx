e8ce713ba24f12a995c7d24a4566390743bbb3e9
Dmitry Ponyatov <dponyatov@gmail.com>
2024-07-01 13:12:24 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/cclibs.mk
+++ mk/cclibs.mk
@@ -4,19 +4,33 @@ GMP_CFG     = $(CCLIBS_CFG) $(CCLIBS_WITH)
 MPFR_CFG    = $(CCLIBS_CFG) $(CCLIBS_WITH)
 MPC_CFG     = $(CCLIBS_CFG) $(CCLIBS_WITH)
 
-cclibs: gmp mpfr mpc
+.PHONY: cclibs gmp mpfr mpc isl cloog
+cclibs: gmp mpfr mpc isl cloog
 
 gmp: $(CROSS)/lib/libgmp.a
 $(CROSS)/lib/libgmp.a: $(TMP)/$(GMP)/README
 	cd $(TMP)/$(GMP); ./$(CFG) $(GMP_CFG) \
-	&& $(MAKE) -j$(CORES) && $(MAKE) install
+	&& $(MAKE) -j$(CORES) && $(MAKE) install-strip
 
 mpfr: $(CROSS)/lib/libmpfr.a
 $(CROSS)/lib/libmpfr.a: $(TMP)/$(MPFR)/README
 	cd $(TMP)/$(MPFR); ./$(CFG) $(MPFR_CFG) \
-	&& $(MAKE) -j$(CORES) && $(MAKE) install
+	&& $(MAKE) -j$(CORES) && $(MAKE) install-strip
 
 mpc: $(CROSS)/lib/libmpc.a
 $(CROSS)/lib/libmpc.a: $(TMP)/$(MPC)/README
 	cd $(TMP)/$(MPC); ./$(CFG) $(MPC_CFG) \
-	&& $(MAKE) -j$(CORES) && $(MAKE) install
+	&& $(MAKE) -j$(CORES) && $(MAKE) install-strip
+
+ISL_CFG   = $(CCLIBS_CFG) --with-gmp-prefix=$(CROSS)
+CLOOG_CFG = $(ISL_CFG)
+
+isl: $(CROSS)/lib/libisl.a
+$(CROSS)/lib/libisl.a: $(TMP)/$(ISL)/README
+	cd $(TMP)/$(ISL); ./$(CFG) $(ISL_CFG) \
+	&& $(MAKE) -j$(CORES) && $(MAKE) install-strip
+
+cloog: $(CROSS)/lib/libcloog-isl.a
+$(CROSS)/lib/libcloog-isl.a: $(TMP)/$(CLOOG)/README
+	cd $(TMP)/$(CLOOG); ./$(CFG) $(CLOOG_CFG) \
+	&& $(MAKE) -j$(CORES) && $(MAKE) install-strip
--- mk/rule.mk
+++ mk/rule.mk
@@ -2,3 +2,5 @@ $(TMP)/%/README: $(GZ)/%.tar.xz
 	cd $(TMP) ; xzcat $< | tar x && touch $@
 $(TMP)/%/README: $(GZ)/%.tar.gz
 	cd $(TMP) ;  zcat $< | tar x && touch $@
+$(TMP)/%/README: $(GZ)/%.tar.bz2
+	cd $(TMP) ; bzcat $< | tar x && touch $@
