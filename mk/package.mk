86499b8d970a459b1e7e88b86e4046250c6dca21
Dmitry Ponyatov <dponyatov@gmail.com>
2024-07-01 09:41:57 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/package.mk
+++ mk/package.mk
@@ -6,7 +6,14 @@ GCC     = gcc-$(GCC_VER)
 GCC_GZ  = $(GCC).tar.xz
 GCC_URL = https://mirror.koddos.net/gcc/releases/$(GCC)
 
-GMP_VER = 6.3.0
 GMP     = gmp-$(GMP_VER)
 GMP_GZ  = $(GMP).tar.xz
 GMP_URL = https://ftp.gnu.org/gnu/gmp
+
+MPFR     = mpfr-$(MPFR_VER)
+MPFR_GZ  = $(MPFR).tar.xz
+MPFR_URL = https://www.mpfr.org/mpfr-current
+
+MPC     = mpc-$(MPC_VER)
+MPC_GZ  = $(MPC).tar.gz
+MPC_URL = https://ftp.gnu.org/gnu/mpc/$(MPC)
--- mk/version.mk
+++ mk/version.mk
@@ -4,3 +4,6 @@ GCC_VER      = 14.1.0
 LINUX_VER    = 
 LIBC_VER     =
 BB_VER       =
+GMP_VER      = 6.3.0
+MPFR_VER     = 4.2.1
+MPC_VER      = 1.3.1
