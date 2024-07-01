35dceff40e3eeeb37f22033bb4122cb7700736b8
Dmitry Ponyatov <dponyatov@gmail.com>
2024-07-01 16:56:00 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/libc.mk
+++ mk/libc.mk
@@ -1,11 +1,15 @@
-LIBC_CFG = CROSS=$(TARGET)- ARCH=$(ARCH) PREFIX=$(ROOT)/libc
+LIBC_CFG = CROSS=$(TARGET)- ARCH=$(ARCH) \
+			PREFIX=$(ROOT)/libc 
 
 .PHONY: libc
 libc: $(TMP)/$(LIBC)/README
 	cd $(TMP)/$(LIBC) ; rm .config ;\
 	$(XPATH) $(MAKE) $(LIBC_CFG) allnoconfig ;\
+	echo 'KERNEL_HEADERS="$(ROOT)/include"' >> .config ;\
+	echo 'CROSS_COMPILER_PREFIX="$(TARGET)-"' >> .config ;\
 	cat $(CWD)/all/all.libc      >> .config ;\
 	cat $(CWD)/cpu/$(CPU).libc   >> .config ;\
 	cat $(CWD)/arch/$(ARCH).libc >> .config ;\
 	cat $(CWD)/app/$(APP).libc   >> .config ;\
-	$(XPATH) $(MAKE) $(LIBC_CFG) menuconfig
+	$(XPATH) $(MAKE) $(LIBC_CFG) menuconfig &&\
+	$(XPATH) $(MAKE) -j$(CORES)
