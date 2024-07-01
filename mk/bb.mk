cde7dbc99c6eec6214d0aecab997693bb995af29
Dmitry Ponyatov <dponyatov@gmail.com>
2024-07-01 19:11:23 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/bb.mk
+++ mk/bb.mk
@@ -1,22 +1,11 @@
-BB_CFG = CROSS_COMPILE=$(TARGET)- PREFIX=$(ROOT)/bb
-
-# CROSS_COMPILE=$(TARGET)- ARCH=$(ARCH) PREFIX=$(ROOT)
-
-# echo 'KERNEL_HEADERS="$(ROOT)/usr/include"' >> .config ;\
-# cat $(CWD)/cpu/$(CPU).libc   >> .config ;\
-# cat $(CWD)/arch/$(ARCH).libc >> .config ;\
-# $(XPATH) $(MAKE) $(LIBC_CFG) -j$(CORES) &&\
-# $(XPATH) $(MAKE) $(LIBC_CFG) install
-
-# echo 'CROSS_COMPILER_PREFIX="$(TARGET)-"' >> .config ;\
+BB_CFG = CROSS_COMPILE=$(TARGET)- PREFIX=$(ROOT)
 
 .PHONY: bb
 bb: $(TMP)/$(BB)/README
 	cd $(TMP)/$(BB) ; rm .config ;\
 	cat $(CWD)/all/all.bb      >> .config ;\
 	$(XPATH) $(MAKE) $(BB_CFG) menuconfig &&\
-	$(XPATH) $(MAKE) $(BB_CFG) -j$(CORES)
-# $(XPATH) $(MAKE) $(BB_CFG) install CONFIG_PREFIX=$(ROOT)/bb
+	$(XPATH) $(MAKE) $(BB_CFG) -j$(CORES) &&\
+	$(XPATH) $(MAKE) $(BB_CFG) install CONFIG_PREFIX=$(ROOT)
 
 # $(XPATH) $(MAKE) $(BB_CFG) allnoconfig ;\
-# cat $(CWD)/app/$(APP).bb   >> .config ;\
