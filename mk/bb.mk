a5186c71345b097afbb6c3764fd5821ee6c19f74
Dmitry Ponyatov <dponyatov@gmail.com>
2024-07-01 18:28:24 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/bb.mk
+++ mk/bb.mk
@@ -14,8 +14,9 @@ BB_CFG = CROSS_COMPILE=$(TARGET)- PREFIX=$(ROOT)/bb
 bb: $(TMP)/$(BB)/README
 	cd $(TMP)/$(BB) ; rm .config ;\
 	cat $(CWD)/all/all.bb      >> .config ;\
-	$(XPATH) $(MAKE) $(BB_CFG) menuconfig
-
+	$(XPATH) $(MAKE) $(BB_CFG) menuconfig &&\
+	$(XPATH) $(MAKE) $(BB_CFG) -j$(CORES)
+# $(XPATH) $(MAKE) $(BB_CFG) install CONFIG_PREFIX=$(ROOT)/bb
 
 # $(XPATH) $(MAKE) $(BB_CFG) allnoconfig ;\
 # cat $(CWD)/app/$(APP).bb   >> .config ;\
