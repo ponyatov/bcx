45716dfef50e838e8149f82123dcba87bf9950bb
Dmitry Ponyatov <dponyatov@gmail.com>
2024-07-01 17:53:29 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/libc.mk
+++ mk/libc.mk
@@ -1,15 +1,16 @@
-LIBC_CFG = CROSS=$(TARGET)- ARCH=$(ARCH) \
-			PREFIX=$(ROOT)/libc 
+LIBC_CFG = CROSS_COMPILE=$(TARGET)- ARCH=$(ARCH) PREFIX=$(ROOT)
 
 .PHONY: libc
 libc: $(TMP)/$(LIBC)/README
 	cd $(TMP)/$(LIBC) ; rm .config ;\
 	$(XPATH) $(MAKE) $(LIBC_CFG) allnoconfig ;\
-	echo 'KERNEL_HEADERS="$(ROOT)/include"' >> .config ;\
-	echo 'CROSS_COMPILER_PREFIX="$(TARGET)-"' >> .config ;\
+	echo 'KERNEL_HEADERS="$(ROOT)/usr/include"' >> .config ;\
 	cat $(CWD)/all/all.libc      >> .config ;\
 	cat $(CWD)/cpu/$(CPU).libc   >> .config ;\
 	cat $(CWD)/arch/$(ARCH).libc >> .config ;\
 	cat $(CWD)/app/$(APP).libc   >> .config ;\
 	$(XPATH) $(MAKE) $(LIBC_CFG) menuconfig &&\
-	$(XPATH) $(MAKE) -j$(CORES)
+	$(XPATH) $(MAKE) $(LIBC_CFG) -j$(CORES) &&\
+	$(XPATH) $(MAKE) $(LIBC_CFG) install
+
+# echo 'CROSS_COMPILER_PREFIX="$(TARGET)-"' >> .config ;\
