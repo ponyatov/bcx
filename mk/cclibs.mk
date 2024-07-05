385734865edc055c699e21a5ad8d95cb6e2fc3be
Dmitry Ponyatov <dponyatov@samsa.local>
2024-07-05 14:29:58 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@samsa.local>

--- mk/cclibs.mk
+++ mk/cclibs.mk
@@ -1,30 +1,5 @@
-# CCLIBS_CFG  = --enable-static --disable-shared
-# CCLIBS_WITH = --with-gmp=$(CROSS) --with-mpfr=$(CROSS) --with-mpc=$(CROSS)
-# 				--with-isl=$(CROSS) --with-cloog=$(CROSS)
-
-# GMP_CFG     = $(CCLIBS_CFG) $(CCLIBS_WITH)
-# MPFR_CFG    = $(CCLIBS_CFG) $(CCLIBS_WITH)
-# MPC_CFG     = $(CCLIBS_CFG) $(CCLIBS_WITH)
-# ISL_CFG     = $(CCLIBS_CFG) --with-gmp-prefix=$(CROSS)
-# CLOOG_CFG   = $(ISL_CFG)
-
 .PHONY: cclibs gmp mpfr mpc isl cloog
 cclibs: gmp mpfr mpc isl cloog
-
-# gmp: $(CROSS)/lib/libgmp.a
-# $(CROSS)/lib/libgmp.a: $(TMP)/$(GMP)/README
-# 	cd $(TMP)/$(GMP); ./$(CFG) $(GMP_CFG) \
-# 	&& $(MAKE) -j$(CORES) && $(MAKE) install-strip
-
-# mpfr: $(CROSS)/lib/libmpfr.a
-# $(CROSS)/lib/libmpfr.a: $(TMP)/$(MPFR)/README
-# 	cd $(TMP)/$(MPFR); ./$(CFG) $(MPFR_CFG) \
-# 	&& $(MAKE) -j$(CORES) && $(MAKE) install-strip
-
-# mpc: $(CROSS)/lib/libmpc.a
-# $(CROSS)/lib/libmpc.a: $(TMP)/$(MPC)/README
-# 	cd $(TMP)/$(MPC); ./$(CFG) $(MPC_CFG) \
-# 	&& $(MAKE) -j$(CORES) && $(MAKE) install-strip
 
 # cd $(TMP)/$(GCC) ; ./contrib/download_prerequisites --graphite
 
@@ -43,3 +18,28 @@ $(TMP)/gcc/isl: $(TMP)/$(ISL)/README
 cloog: $(TMP)/gcc/cloog
 $(TMP)/gcc/cloog: $(TMP)/$(CLOOG)/README
 	mkdir -p $(TMP)/gcc ; ln -fs $(TMP)/$(CLOOG) $@ && touch $@
+
+# CCLIBS_CFG  = --enable-static --disable-shared
+# CCLIBS_WITH = --with-gmp=$(CROSS) --with-mpfr=$(CROSS) --with-mpc=$(CROSS)
+# 				--with-isl=$(CROSS) --with-cloog=$(CROSS)
+
+# GMP_CFG     = $(CCLIBS_CFG) $(CCLIBS_WITH)
+# MPFR_CFG    = $(CCLIBS_CFG) $(CCLIBS_WITH)
+# MPC_CFG     = $(CCLIBS_CFG) $(CCLIBS_WITH)
+# ISL_CFG     = $(CCLIBS_CFG) --with-gmp-prefix=$(CROSS)
+# CLOOG_CFG   = $(ISL_CFG)
+
+# gmp: $(CROSS)/lib/libgmp.a
+# $(CROSS)/lib/libgmp.a: $(TMP)/$(GMP)/README
+# 	cd $(TMP)/$(GMP); ./$(CFG) $(GMP_CFG) \
+# 	&& $(MAKE) -j$(CORES) && $(MAKE) install-strip
+
+# mpfr: $(CROSS)/lib/libmpfr.a
+# $(CROSS)/lib/libmpfr.a: $(TMP)/$(MPFR)/README
+# 	cd $(TMP)/$(MPFR); ./$(CFG) $(MPFR_CFG) \
+# 	&& $(MAKE) -j$(CORES) && $(MAKE) install-strip
+
+# mpc: $(CROSS)/lib/libmpc.a
+# $(CROSS)/lib/libmpc.a: $(TMP)/$(MPC)/README
+# 	cd $(TMP)/$(MPC); ./$(CFG) $(MPC_CFG) \
+# 	&& $(MAKE) -j$(CORES) && $(MAKE) install-strip
