ea40049847b5d75fd70db7e34348ffff8f38f07d
Dmitry Ponyatov <dponyatov@gmail.com>
2024-07-07 00:34:40 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/cclibs.mk
+++ mk/cclibs.mk
@@ -1,56 +1,37 @@
-.PHONY: cclibs gmp mpfr mpc isl cloog
-cclibs: gmp mpfr mpc isl cloog
-
-# cd $(TMP)/$(GCC) ; ./contrib/download_prerequisites --graphite
-
-# gmp: $(TMP)/gcc/gmp
-# $(TMP)/gcc/gmp: $(TMP)/$(GMP)/README
-# 	mkdir -p $(TMP)/gcc ; ln -fs $(TMP)/$(GMP)   $@ && touch $@
-# mpfr: $(TMP)/gcc/mpfr
-# $(TMP)/gcc/mpfr: $(TMP)/$(MPFR)/README
-# 	mkdir -p $(TMP)/gcc ; ln -fs $(TMP)/$(MPFR)  $@ && touch $@
-# mpc: $(TMP)/gcc/mpc
-# $(TMP)/gcc/mpc: $(TMP)/$(MPC)/README
-# 	mkdir -p $(TMP)/gcc ; ln -fs $(TMP)/$(MPC)   $@ && touch $@
-# isl: $(TMP)/gcc/isl
-# $(TMP)/gcc/isl: $(TMP)/$(ISL)/README
-# 	mkdir -p $(TMP)/gcc ; ln -fs $(TMP)/$(ISL)   $@ && touch $@
-# cloog: $(TMP)/gcc/cloog
-# $(TMP)/gcc/cloog: $(TMP)/$(CLOOG)/README
-# 	mkdir -p $(TMP)/gcc ; ln -fs $(TMP)/$(CLOOG) $@ && touch $@
-
 CCLIBS_CFG  = --enable-static --disable-shared
-CCLIBS_WITH = --with-gmp=$(CROSS) --with-mpfr=$(CROSS) --with-mpc=$(CROSS)
+CCLIBS_WITH = --with-gmp=$(CROSS) --with-mpfr=$(CROSS) --with-mpc=$(CROSS) \
+				--with-isl=$(CROSS) --with-cloog=$(CROSS)
 
 GMP_CFG     = $(CCLIBS_CFG) $(CCLIBS_WITH)
 MPFR_CFG    = $(CCLIBS_CFG) $(CCLIBS_WITH)
 MPC_CFG     = $(CCLIBS_CFG) $(CCLIBS_WITH)
-# ISL_CFG     = $(CCLIBS_CFG) --with-gmp-prefix=$(CROSS)
-# CLOOG_CFG   = $(ISL_CFG)
+ISL_CFG     = $(CCLIBS_CFG) --with-gmp-prefix=$(CROSS)
+CLOOG_CFG   = $(ISL_CFG)
+
+.PHONY: cclibs gmp mpfr mpc isl cloog
+cclibs:        gmp mpfr mpc isl cloog
 
 gmp: $(CROSS)/lib/libgmp.a
 $(CROSS)/lib/libgmp.a:   $(TMP)/$(GMP)/README
-	cd $(TMP)/$(GMP);   ./$(CFG) $(GMP_CFG) \
-	&& $(MAKE) -j$(CORES) && $(MAKE) install-strip
+	cd $(TMP)/$(GMP);   ./$(CFG) $(GMP_CFG) &&\
+	$(MAKE) -j$(CORES) && $(MAKE) install-strip
 
 mpfr: $(CROSS)/lib/libmpfr.a
 $(CROSS)/lib/libmpfr.a: $(TMP)/$(MPFR)/README
-	cd $(TMP)/$(MPFR);  ./$(CFG) $(MPFR_CFG) \
-	&& $(MAKE) -j$(CORES) && $(MAKE) install-strip
+	cd $(TMP)/$(MPFR);  ./$(CFG) $(MPFR_CFG) &&\
+	$(MAKE) -j$(CORES) && $(MAKE) install-strip
 
 mpc: $(CROSS)/lib/libmpc.a
 $(CROSS)/lib/libmpc.a:  $(TMP)/$(MPC)/README
-	cd $(TMP)/$(MPC);   ./$(CFG) $(MPC_CFG) \
-	&& $(MAKE) -j$(CORES) && $(MAKE) install-strip
+	cd $(TMP)/$(MPC);   ./$(CFG) $(MPC_CFG) &&\
+	$(MAKE) -j$(CORES) && $(MAKE) install-strip
 
-isl:   $(TMP)/$(GCC)/isl   $(TMP)/$(BINUTILS)/isl
-$(TMP)/$(GCC)/isl:         $(TMP)/$(ISL)/README   $(TMP)/$(GCC)/README
-	cp -r $(TMP)/$(ISL) $@
-$(TMP)/$(BINUTILS)/isl:    $(TMP)/$(ISL)/README   $(TMP)/$(BINUTILS)/README
-	cp -r $(TMP)/$(ISL) $@
+isl: $(CROSS)/lib/libisl.a
+$(CROSS)/lib/libisl.a:  $(TMP)/$(ISL)/README
+	cd $(TMP)/$(ISL);   ./$(CFG) $(ISL_CFG) &&\
+	$(MAKE) -j$(CORES) && $(MAKE) install-strip
 
-cloog: $(TMP)/$(GCC)/cloog $(TMP)/$(BINUTILS)/cloog
-$(TMP)/$(GCC)/cloog:       $(TMP)/$(CLOOG)/README $(TMP)/$(GCC)/README
-	cp -r $(TMP)/$(CLOOG) $@
-$(TMP)/$(BINUTILS)/cloog:  $(TMP)/$(CLOOG)/README $(TMP)/$(BINUTILS)/README
-	cp -r $(TMP)/$(CLOOG) $@
+cloog: $(CROSS)/lib/libcloog-isl.a
+$(CROSS)/lib/libcloog-isl.a:  $(TMP)/$(CLOOG)/README
+	cd $(TMP)/$(CLOOG);   ./$(CFG) $(CLOOG_CFG) &&\
+	$(MAKE) -j$(CORES) && $(MAKE) install-strip
--- mk/gcc.mk
+++ mk/gcc.mk
@@ -4,7 +4,6 @@ BINUTILS_CFG = --target=$(TARGET) --with-sysroot=$(ROOT) --disable-bootstrap \
 				--enable-lto --disable-multilib \
 				--enable-ld=default --enable-gold --enable-plugins \
                 $(CCLIBS_WITH)
-# configuring in isl error:
 
 GCC0_CFG     = --disable-shared --disable-threads \
 				--without-headers --with-newlib   \
@@ -13,29 +12,17 @@ GCC0_CFG     = --disable-shared --disable-threads \
 GCC_CFG      = --enable-shared --enable-threads --enable-libgomp \
 				--enable-languages="c,c++"
 
-# https://raghunathlolur.wordpress.com/2014/06/30/combined-tree-build-of-gcc-binutils-and-libraries/
-
 .PHONY: cross0 cross
 
-# cross0: $(TMP)/gcc/gcc cclibs
-# 	rm -rf $(TMP)/gcc-build ; mkdir $(TMP)/gcc-build ; cd $(TMP)/gcc-build ;\
-# 	$(XPATH) $(TMP)/gcc/$(CFG) $(BINUTILS_CFG) $(GCC0_CFG)
-# 	$(MAKE) gcc
-
-cross0: $(TMP)/$(GCC)/README cclibs
+cross0: $(TMP)/$(GCC)/README cclibs binutils
 	rm -rf $(TMP)/gcc-build ; mkdir $(TMP)/gcc-build ; cd $(TMP)/gcc-build ;\
 	$(XPATH) $(TMP)/$(GCC)/$(CFG) $(BINUTILS_CFG) $(GCC0_CFG)
 	$(XPATH) $(MAKE) gcc
 
-# cross: $(TMP)/gcc/gcc cclibs
-# 	rm -rf $(TMP)/gcc-build ; mkdir $(TMP)/gcc-build ; cd $(TMP)/gcc-build ;\
-# 	$(XPATH) $(TMP)/gcc/$(CFG) $(BINUTILS_CFG) $(GCC_CFG)
-# # $(MAKE) gcc
-
 cross: $(TMP)/$(GCC)/README cclibs
 	rm -rf $(TMP)/gcc-build ; mkdir $(TMP)/gcc-build ; cd $(TMP)/gcc-build ;\
 	$(XPATH) $(TMP)/$(GCC)/$(CFG) $(BINUTILS_CFG) $(GCC_CFG)
-# $(MAKE) gcc
+	$(MAKE) gcc
 
 .PHONY: gcc
 gcc:
@@ -50,19 +37,6 @@ gpp:
 	cd $(TMP)/gcc-build ;\
 	$(XPATH) $(MAKE) -j$(CORES)     all-target-libstdc++-v3 &&\
 	$(XPATH) $(MAKE)            install-target-libstdc++-v3
-
-$(TMP)/gcc/binutils: $(TMP)/$(BINUTILS)/README
-	mkdir -p $(TMP)/gcc ; cd $(TMP)/gcc ;\
-	ln -fs $(TMP)/$(BINUTILS)/* . && touch $@
-$(TMP)/gcc/gcc:      $(TMP)/$(GCC)/README
-	mkdir -p $(TMP)/gcc ; cd $(TMP)/gcc ;\
-	ln -fs $(TMP)/$(GCC)/*      . && touch $@
-
-# .PHONY: binutils
-# binutils:
-# 	cd $(TMP)/gcc-build ;\
-# 	$(XPATH) $(MAKE) -j$(CORES)     all-binutils     all-ld     all-gas   &&\
-# 	$(XPATH) $(MAKE)            install-binutils install-ld install-gas
 
 .PHONY: binutils
 binutils: $(CROSS)/bin/$(TARGET)-ld
