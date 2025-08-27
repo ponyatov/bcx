72498788497917a84811f7d06dd788da7152f71e
Dmitry Ponyatov <dponyatov@gmail.com>
2025-08-27 14:50:55 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/cross.mk
+++ mk/cross.mk
@@ -5,6 +5,9 @@ include  cpu/$(CPU)/$(CPU).mk
 include arch/$(ARCH)/$(ARCH).mk
 include   os/$(OS)/$(OS).mk
 
+XPATH = PATH=$(CROSS)/bin:$(PATH)
+CFG   = configure --prefix=$(CROSS)
+
 .PHONY: cross
 cross: $(CROSS)/.gitignore $(ROOT)/.gitignore binutils
 $(CROSS)/.gitignore: bin/.gitignore
@@ -12,13 +15,18 @@ $(CROSS)/.gitignore: bin/.gitignore
 $(ROOT)/.gitignore: bin/.gitignore
 	mkdir -p $(dir $@) ; cp $< $@
 
+TLD = $(CROSS)/bin/$(TARGET)-ld
+TCC = $(CROSS)/bin/$(TARGET)-gcc
+
 .PHONY: binutils
 
 BINUTILS_CFG += --disable-nls --target=$(TARGET)
 BINUTILS_CFG += --with-sysroot=$(ROOT) --with-native-system-header-dir=/include
 BINUTILS_CFG += --enable-lto --disable-multilib
 
-binutils: $(HOME)/src/$(BINUTILS)/README
+binutils: $(TLD)
+$(TLD): $(HOME)/src/$(BINUTILS)/README
 	rm -rf $(TMP)/$(BINUTILS) ; mkdir $(TMP)/$(BINUTILS) ;\
 	cd $(TMP)/$(BINUTILS) ;\
-	$(HOME)/src/$(BINUTILS)/configure $(BINUTILS_CFG)
+	$(XPATH) $(HOME)/src/$(BINUTILS)/$(CFG) $(BINUTILS_CFG) &&\
+	$(MAKE) -j$(CORES) && $(MAKE) install-strip
