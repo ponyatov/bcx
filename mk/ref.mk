16b5f8c4e2c166237039c0519c3302ca72a0cfe6
Dmitry Ponyatov <dponyatov@gmail.com>
2025-08-27 15:10:54 +0400

    .

--- mk/ref.mk
+++ mk/ref.mk
@@ -8,7 +8,13 @@ $(HOME)/src/$(BINUTILS)/README: /usr/src/binutils/$(BINUTILS_GZ)
 	mkdir -p $(HOME)/src ; cd $(HOME)/src ;\
 	xzcat $< | tar x && touch $@
 
+# $(HOME)/src/$(BINUTILS)/README: $(DISTR)/$(BINUTILS_GZ)
+# 	cd $(REF) ; xzcat $< | tar x && touch $@
+
 REF += $(HOME)/src/$(GCC)/README
 $(HOME)/src/$(GCC)/README: /usr/src/gcc-12/$(GCC_GZ)
 	mkdir -p $(HOME)/src ; cd $(HOME)/src ;\
 	xzcat $< | tar x && touch $@
+
+# $(HOME)/src/$(GCC)/README: $(DISTR)/$(GCC_GZ)
+# 	cd $(REF) ; xzcat $< | tar x && touch $@
