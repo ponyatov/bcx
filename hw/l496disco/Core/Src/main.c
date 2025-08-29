2f8cfa7f1f8ed97ed86561da44b8f35390d68180
Dmitry Ponyatov <dponyatov@gmail.com>
2025-08-29 17:20:31 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- hw/l496disco/Core/Src/main.c
+++ hw/l496disco/Core/Src/main.c
@@ -105,7 +105,6 @@ int main(void)
   // HAL_UART_Transmit(&huart2, (uint8_t*)hello, sizeof(hello), HAL_MAX_DELAY);
   // HAL_Delay(1000);
   while (1) {
-      /* USER CODE END WHILE */
       uint32_t start_time = HAL_GetTick();
       //
       uint8_t byte;
@@ -120,6 +119,8 @@ int main(void)
       sprintf(time_msg, "test time: %lu ms\n", elapsed_time);
       HAL_UART_Transmit(&huart2, (uint8_t *)time_msg, strlen(time_msg),
                         HAL_MAX_DELAY);
+      /* USER CODE END WHILE */
+    /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
 }
