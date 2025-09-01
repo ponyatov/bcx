a2a591f438a1a04e51e9d836d2449068003dab12
Dmitry Ponyatov <dponyatov@gmail.com>
2025-09-01 15:53:25 +0400

    .

--- mk/patch.mk
+++ mk/patch.mk
@@ -10,7 +10,7 @@ all:
 	dos2unix $(FILES)
 	$(MAKE) -f $(MAKEFILE_LIST) $(FIXES)
 %.fix: %
-	cp $@ $<
-# 	meld $< $@
+#	cp $@ $<
+ 	meld $< $@
 # 	patch -u $< $<.patch && touch $@
 # rm Core/Src/syscalls.c
